bd8c9050483ece46d709bdd0d2aafc410e64dbbf
DaRkLord0350 <anshumanjha0350@gmail.com>
2025-06-14 07:34:14 +0530

    fixing paitent registeration page

---
+++ app/actions/appointment.js
@@ -0,0 +1,108 @@
+"use server";
+
+import db from "@/lib/db";
+import { AppointmentSchema, VitalSignsSchema } from "@/lib/schema";
+import { auth } from "@clerk/nextjs/server";
+
+// Create a new appointment
+export async function createNewAppointment(data) {
+  try {
+    const validatedData = AppointmentSchema.safeParse(data);
+
+    if (!validatedData.success) {
+      return { success: false, msg: "Invalid data" };
+    }
+    const validated = validatedData.data;
+
+    await db.appointment.create({
+      data: {
+        patient_id: data.patient_id,
+        doctor_id: validated.doctor_id,
+        time: validated.time,
+        type: validated.type,
+        appointment_date: new Date(validated.appointment_date),
+        note: validated.note,
+      },
+    });
+
+    return {
+      success: true,
+      message: "Appointment booked successfully",
+    };
+  } catch (error) {
+    console.log(error);
+    return { success: false, msg: "Internal Server Error" };
+  }
+}
+
+// Update appointment status and reason
+export async function appointmentAction(
+  id,
+  status,
+  reason
+) {
+  try {
+    await db.appointment.update({
+      where: { id: Number(id) },
+      data: {
+        status,
+        reason,
+      },
+    });
+
+    return {
+      success: true,
+      error: false,
+      msg: `Appointment ${status.toLowerCase()} successfully`,
+    };
+  } catch (error) {
+    console.log(error);
+    return { success: false, msg: "Internal Server Error" };
+  }
+}
+
+// Add vital signs to an appointment
+export async function addVitalSigns(
+  data,
+  appointmentId,
+  doctorId
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return { success: false, msg: "Unauthorized" };
+    }
+
+    const validatedData = VitalSignsSchema.parse(data);
+
+    let medicalRecord = null;
+
+    if (!validatedData.medical_id) {
+      medicalRecord = await db.medicalRecords.create({
+        data: {
+          patient_id: validatedData.patient_id,
+          appointment_id: Number(appointmentId),
+          doctor_id: doctorId,
+        },
+      });
+    }
+
+    const med_id = validatedData.medical_id || medicalRecord?.id;
+
+    await db.vitalSigns.create({
+      data: {
+        ...validatedData,
+        medical_id: Number(med_id),
+      },
+    });
+
+    return {
+      success: true,
+      msg: "Vital signs added successfully",
+    };
+  } catch (error) {
+    console.log(error);
+    return { success: false, msg: "Internal Server Error" };
+  }
+}
